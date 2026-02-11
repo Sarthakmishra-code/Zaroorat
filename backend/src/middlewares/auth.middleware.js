381626c7afa57785c9f42dc4e05cb5fb60e9bff9
Sarthakmishra-code <sarthakmishra301106@gmail.com>
2026-02-12 02:31:19 +0530

    added auth middleware

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,27 @@
+import { User } from "../models/user.model";
+import ApiError from "../utils/ApiError";
+import { asyncHandler } from "../utils/asyncHandler";
+import jwt from "jsonwebtoken"
+
+const verifyJWT = asyncHandler(async(req, res, next) => {
+    try {
+        const token = req.cookies?.accessToken || req.header("AUthorization")?.replace("Bearer ", "");
+        
+        if(!token){
+            throw new ApiError(401, "Unautorized Access")
+        }
+    
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+    
+        if(!user){
+            throw new ApiError(401, "Invalid Access Token")
+        }
+    
+        req.user = user;
+        next();
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid access token")
+    }
+})
