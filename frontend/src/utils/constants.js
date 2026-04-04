e456ab94a5855ade4c7cdfc357c0311a442f66a4
Sarthakmishra-code <sarthakmishra301106@gmail.com>
2026-04-05 00:17:36 +0530

    Adding backend/render url

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api/v1';
+export const API_URL = import.meta.env.VITE_API_URL || 'https://zaroorat.onrender.com/api/v1';
 
 export const CITIES = [
   {
